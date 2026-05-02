f256d465a4a9989cdbb2a4fc06dd24371f788b14
zobayerHosen <zobayerhosen2345@gmail.com>
2026-05-02 05:27:09 -0600

    optimized the descripiton showing ui and add google social login api implement

--- components/auth/GoogleLogin.jsx
+++ components/auth/GoogleLogin.jsx
@@ -1,17 +1,21 @@
 "use client"
 import { FcGoogle } from "react-icons/fc";
+import { useGoogleSocialLogin } from "@/hooks/useGoogleSocialLogin";
+
 const GoogleLogin = () => {
-
-    const signInWithGoogle = () => {
-
-    }
-
+    const { googleLogin, isPending } = useGoogleSocialLogin();
 
     // main render
     return (
-        <button type="button" onClick={signInWithGoogle} className="w-full cursor-pointer rounded-full py-4 px-3 bg-[#DADAD1] flex items-center justify-center gap-1">
+        <button
+            type="button"
+            onClick={() => googleLogin()}
+            disabled={isPending}
+            className={`w-full cursor-pointer rounded-full py-4 px-3 bg-[#DADAD1] flex items-center justify-center gap-1 transition-opacity duration-200 ${isPending ? "opacity-50 cursor-not-allowed" : "hover:opacity-90"
+                }`}
+        >
             <FcGoogle size={20} />
-            <span>Sign In with Google</span>
+            <span>{isPending ? "Signing In..." : "Sign In with Google"}</span>
         </button>
     )
 }
